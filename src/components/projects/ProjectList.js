d71055654cf09b9c5503d8588140ab57fbbddd7d
promehedi <mmhasan563@gmail.com>
2021-03-12 18:02:13 +0600

    Project Summary Component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,57 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
   return (
     <div className='project-list section'>
-      <div className='card hoverable purple darken-2 project-summary'>
-        <div className='card-content white-text'>
-          <span className='card-title'>Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <br />
-          <p>Posted by Mehedi</p>
-          <p className='grey-text'>3rd September, 2pm</p>
-        </div>
-        <div class='card-action'>
-          <Link>Read More</Link>
-        </div>
-      </div>
-      <div className='card hoverable purple darken-2 project-summary'>
-        <div className='card-content white-text'>
-          <span className='card-title'>Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <br />
-          <p>Posted by Mehedi</p>
-          <p className='grey-text'>3rd September, 2pm</p>
-        </div>
-        <div class='card-action'>
-          <Link>Read More</Link>
-        </div>
-      </div>
-      <div className='card hoverable purple darken-2 project-summary'>
-        <div className='card-content white-text'>
-          <span className='card-title'>Project Title</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-          <br />
-          <p>Posted by Mehedi</p>
-          <p className='grey-text'>3rd September, 2pm</p>
-        </div>
-        <div class='card-action'>
-          <Link>Read More</Link>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
