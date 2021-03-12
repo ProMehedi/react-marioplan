f1d1cde8060088b131e553c8b589182325998245
promehedi <mmhasan563@gmail.com>
2021-03-12 17:24:10 +0600

    Add React-Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <h1>Hello 3!</h1>
-    </div>
+    </Router>
   )
 }
 
